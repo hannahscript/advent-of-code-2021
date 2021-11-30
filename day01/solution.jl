1ef310ee0debdf493fabeca05ed79e9bc5d6f5c2
hannahscript <h4nn4h777@protonmail.com>
2021-11-30 01:30:01 +0100

    Basic structure

---
+++ day01/solution.jl
@@ -0,0 +1,12 @@
+input = read("input.txt", String)
+
+function part1()
+    return input
+end
+
+function part2()
+    return input
+end
+
+println("Part 1: " * part1())
+println("Part 2: " * part2())
