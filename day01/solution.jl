361f58130c6de3d36ebb8a826d08908892bc6d06
hannahscript <h4nn4h777@protonmail.com>
2021-12-01 10:44:00 +0100

    Solve day 01

--- day01/solution.jl
+++ day01/solution.jl
@@ -1,12 +1,35 @@
-input = read("input.txt", String)
-
-function part1()
-    return input
+function getinput()
+    input = read("input.txt", String)
+    lines = split(input, "\n")
+    return map(line -> parse(Int64, line), lines)
 end
 
-function part2()
-    return input
+function part1(depths)
+    increases = 0
+    for i = 2:length(depths)
+        if depths[i - 1] < depths[i]
+            increases += 1
+        end
+    end
+
+    return increases
 end
 
-println("Part 1: " * part1())
-println("Part 2: " * part2())
+function part2(depths)
+    increases = 0
+    prevwindow = sum(depths[1:3])
+    for i = 4:length(depths)
+        window = prevwindow - depths[i - 3] + depths[i]
+        if window > prevwindow
+            increases += 1
+        end
+
+        prevwindow = window
+    end
+
+    return increases
+end
+
+depths = getinput()
+println("Part 1: " * string(part1(depths)))
+println("Part 2: " * string(part2(depths)))
