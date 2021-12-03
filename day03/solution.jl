4e75745240c40110d32113e12f563155aeae4bbb
hannahscript <h4nn4h777@protonmail.com>
2021-12-03 20:19:41 +0100

    Solved day 03

---
+++ day03/solution.jl
@@ -0,0 +1,44 @@
+function getinput()
+    input = read("input.txt", String)
+    lines = filter(line -> !isempty(line), split(input, "\r\n"))
+    return map(line -> map(c -> parse(Int, c), split(line, "")), lines)
+end
+
+function find_most_common(report)
+    reportlen_half = length(report) / 2
+    return map(n -> n == reportlen_half ? 1 : n > reportlen_half, sum(report))
+end
+
+function part1(most_common)
+    most_common = find_most_common(report)
+    rates = [0, 0]
+    for (i, d) in enumerate(reverse(most_common))
+        rates[d + 1] += 2 ^ (i - 1)
+    end
+
+    return prod(rates)
+end
+
+function part2(report)
+    ratings = [0, 0]
+
+    for r = 1:2
+        leftovers = report
+        most_common = find_most_common(leftovers)
+        for i in 1:length(most_common)
+            leftovers = filter(line -> r == 1 ? line[i] == most_common[i] : line[i] != most_common[i], leftovers)
+            if (length(leftovers) == 1)
+                ratings[r] = parse(Int, join(leftovers[1],""), base=2)
+                break
+            end
+
+            most_common = find_most_common(leftovers)
+        end
+    end
+
+    return prod(ratings)
+end
+
+report = getinput()
+println("Part 1: $(part1(report))") # 1092896
+println("Part 2: $(part2(report))") # 4672151
